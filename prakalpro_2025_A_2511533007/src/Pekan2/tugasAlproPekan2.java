349223697bf8958abc942380b56ab1fde5ef2830
DELL <DELL@DESKTOP-JURI4VG>
2025-09-21 23:46:09 +0700

    Tugas Alpro

---
+++ prakalpro_2025_A_2511533007/src/Pekan2/tugasAlproPekan2.java
@@ -0,0 +1,26 @@
+package Pekan2;
+
+public class tugasAlproPekan2 {
+
+	public static void main(String[] args) {
+		
+		//Program data nilai tugas pekan 2
+		System.out.println("Nilai Tugas Pekan 2 ");
+		System.out.println("");
+		
+		int absen;  //absen ke berapa
+		absen = 11;
+		float nilai;  //nilai tugas
+		nilai = 81.1f;
+		char kelas;  //Kelas A atau B
+		kelas = 'A';
+		boolean sudahKumpul;  //sudahkah mengumpulkan tugas
+		sudahKumpul = true;
+	
+		
+		System.out.println("Absen: "+absen);
+		System.out.println("Nilai: "+nilai);
+		System.out.println("Kelas: "+kelas);
+		System.out.println("Sudah Kumpul: "+sudahKumpul);
+	}
+}
