0e4b09f04cc4115e8d7536bc355f24fb83b63098
DELL <DELL@DESKTOP-JURI4VG>
2025-09-29 14:25:22 +0700

    Pekan4 kelasA

---
+++ prakalpro_2025_A_2511533007/src/Pekan4/latIfl.java
@@ -0,0 +1,19 @@
+package Pekan4;
+import java.util.Scanner;
+public class latIfl {
+	
+
+	public static void main(String[] args) {
+		double IPK;
+		Scanner input=new Scanner (System.in);
+		System.out.println("Input IPK Anda = ");
+		IPK=input.nextDouble();
+		input.close();
+		if (IPK>2.75) {
+			System.out.println("Anda Lulus Sangat Memuaskan dengan IPK "+IPK);
+		}
+
+	}
+
+}
+
