52aa3a8d84212dc4d400f52283a0665a98c58ea4
DELL <DELL@DESKTOP-JURI4VG>
2025-10-27 14:34:39 +0700

    Merge branch 'master' of https://github.com/Rifkix/2025_prakalpro_A_2511533007.git

---
+++ prakalpro_2025_A_2511533007/src/Pekan5/TugasPekan5.java
@@ -0,0 +1,16 @@
+package Pekan5;
+
+public class TugasPekan5 {
+	public static void main(String[] args) {
+		for (int line = 1; line <= 5; line++) {
+			for (int j = 1; j <= -1 * line + 5; j++) {
+				System.out.print(".");
+				}
+			System.out.print(line);
+			for (int j = 1; j <= line - 1; j++) {
+				System.out.print(".");
+				}
+			System.out.println();
+	        }
+	    }
+	}
