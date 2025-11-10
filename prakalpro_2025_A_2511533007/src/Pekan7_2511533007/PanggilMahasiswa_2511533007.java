a8357b420bf164eefab789fa7ad8b6a9b321dc7d
Rifkix <230964151+Rifkix@users.noreply.github.com>
2025-11-10 14:44:33 +0700

    Add files via upload

---
+++ prakalpro_2025_A_2511533007/src/Pekan7_2511533007/PanggilMahasiswa_2511533007.java
@@ -0,0 +1,16 @@
+package Pekan7_2511533007;
+
+public class PanggilMahasiswa_2511533007 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511533007 a = new Mahasiswa_2511533007();
+		a.setNim(23532);
+		a.setNama("Rahmat");
+		System.out.println(a.getNim());
+		System.out.println(a.getNama());
+		a.Cetak();
+				
+
+	}
+
+}
